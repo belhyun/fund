c8ad572f0496bd293648e0476ada6b8e0b1660e2
JongHyun <belhyun@yonsei.ac.kr>
2018-08-29 17:36:05 +0900

    add express admin

---
+++ src/test/java/com/yonsei/fund/model/fund/entity/FundCardTest.java
@@ -0,0 +1,22 @@
+package com.yonsei.fund.model.fund.entity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FundCardTest {
+
+    @Test
+    public void build_test() {
+        FundCard fundCard = FundCard.builder()
+                .targetAmount(BigDecimal.TEN).build();
+
+        assertNull(fundCard.getFundCardDonations());
+    }
+
+}
