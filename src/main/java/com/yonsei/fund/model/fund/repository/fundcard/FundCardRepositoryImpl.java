266a7d9a6c5f255447e26ed4015085278f6dec9f
JongHyun <belhyun@yonsei.ac.kr>
2018-09-08 20:46:17 +0900

    quiery dsl

--- src/main/java/com/yonsei/fund/model/fund/repository/fundcard/FundCardRepositoryImpl.java
+++ src/main/java/com/yonsei/fund/model/fund/repository/fundcard/FundCardRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.querydsl.jpa.JPQLQuery;
 import com.yonsei.fund.controller.fundcard.FundCardCondition;
 import com.yonsei.fund.model.fund.entity.FundCard;
 import com.yonsei.fund.model.fund.entity.QFundCard;
+import com.yonsei.fund.model.fund.entity.QFundCardPhoto;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.time.LocalDateTime;
@@ -17,12 +18,15 @@ public class FundCardRepositoryImpl extends QuerydslRepositorySupport implements
 
     private final QFundCard qFundCard = QFundCard.fundCard;
 
+    private final QFundCardPhoto qFundCardPhoto = QFundCardPhoto.fundCardPhoto;
+
 
     @Override
     public List<FundCard> getFundCardList(FundCardCondition condition) {
         JPQLQuery<FundCard> query = from(qFundCard)
                 .where(qFundCard.startedAt.loe(LocalDateTime.now()))
-                .where(qFundCard.endedAt.goe(LocalDateTime.now()));
+                .where(qFundCard.endedAt.goe(LocalDateTime.now()))
+                .orderBy(qFundCard.id.desc());
         return query.fetch();
     }
 }
